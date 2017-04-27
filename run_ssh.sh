833054d75360531d713bc8d551a2b7e95a3c29ba
Marcello Romani <marcello.romani@magentys.io>
2017-04-27 11:41:04 +0100

    don't start bash when running as ssh server

--- run_ssh.sh
+++ run_ssh.sh
@@ -10,4 +10,4 @@ echo
 echo "To connect with key-based auth:"
 echo "ssh -i `pwd`/files/id_rsa -p $SSH_PORT admin@localhost"
 
-docker run --rm -it -p $SSH_PORT:22 $IMAGE_NAME /bin/sh -c 'sudo service ssh start && /bin/bash'
+docker run --rm -it -p $SSH_PORT:22 $IMAGE_NAME /bin/sh -c 'sudo service ssh start && sleep infinity'
